d964545e94c6418354328c9822d56b34158151b1
sus_Juice <118504355+Sushant-778@users.noreply.github.com>
2025-04-18 05:35:49 +0000

    function clousers

---
+++ JS Practice/app.js
@@ -0,0 +1,14 @@
+console.log('yooolo');
+
+
+function one(){
+    const userName='Sushant'
+    
+     function two(){
+        const website='youtube'
+        console.log('username');
+     } 
+    //  console.log(website)
+     two();
+}
+one();
